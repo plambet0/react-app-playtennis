5230ab9de2284e43e1a4eb2c22649d2d9a8e0b37
Plamen Uzunov <plamen.uzunov@strypes.eu>
2022-06-29 20:29:57 +0300

    initial create of the app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from '@mui/material'
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Test</Button>
+    </div>
+  );
+}
+
+export default App;
